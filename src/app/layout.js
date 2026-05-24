ef620949d004aaf843fbe32aa4cdea17519e25e4
Mahabub Islam <marufhasan7702@gmail.com>
2026-05-25 03:09:13 +0600

    Proxy setup

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,6 @@
+// import dns from "node:dns";
+// dns.servers(["8.8.8.8","8.8.4.4"]);
+
 import {  Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 
---
+++ src/proxy.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { auth } from "./lib/auth";
+import { headers } from "next/headers";
+
+export async function proxy(request) {
+    // return NextResponse.next();
+//   return NextResponse.redirect(new URL('/login', request.url))
+
+const session = await auth.api.getSession({
+        headers: await headers()
+    })
+
+// const isLoggedIn =false;
+    if(session){
+        return NextResponse.next();
+    }
+    return NextResponse.redirect(new URL('/login', request.url))
+}
+
+export const config = {
+  matcher: ['/career','/news/:path*'],
+}
